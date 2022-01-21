8e3856296d775706da8fb701cedb962bbb4b5161
rohiddev <rohiddev@gmail.com>
2022-01-20 20:08:20 -0700

    Update main.go

--- main.go
+++ main.go
@@ -9,6 +9,23 @@ import (
 
 func main() {
 	r := gin.Default()
+		r.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
+
+		// your custom format
+		return fmt.Sprintf("%s - [%s] \"%s %s %s %d %s \"%s\" %s\"\n",
+			param.ClientIP,
+			param.TimeStamp.Format(time.RFC1123),
+			param.Method,
+			param.Path,
+			param.Request.Proto,
+			param.StatusCode,
+			param.Latency,
+			param.Request.UserAgent(),
+			param.ErrorMessage,
+		)
+	}))
+	r.Use(gin.Recovery())
+	
 
 	db := models.SetupModels() // new
 
